ad925f35c4b5657d3ce3b20855efe0da006816cf
siddheshdhumatkar423 <siddheshdhumatkar423@gmail.com>
2025-04-20 19:23:47 +0530

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,8 +1,10 @@
-from myapp.app import app
-
+from app import app
 
 def test_home():
     tester = app.test_client()
     response = tester.get('/')
     assert response.status_code == 200
     assert b"Hello from Flask" in response.data
+
+
+
