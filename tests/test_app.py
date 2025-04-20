05e0ac6a114c8497c8a0260691976b63e524502f
siddheshdhumatkar423 <siddheshdhumatkar423@gmail.com>
2025-04-20 19:14:15 +0530

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,24 +1,7 @@
-# Use official Python image
-FROM python:3.11-slim
+from app import app
 
-# Set working directory
-WORKDIR /app
-
-# Copy dependencies file first
-COPY requirements.txt .
-
-# Install Python dependencies
-RUN pip install --no-cache-dir -r requirements.txt
-
-# Copy the rest of your application code
-COPY . .
-
-# Set environment variables
-ENV FLASK_APP=app.py
-ENV FLASK_RUN_HOST=0.0.0.0
-
-# Expose the Flask default port
-EXPOSE 5000
-
-# Start the Flask app
-CMD ["flask", "run"]
+def test_home():
+    tester = app.test_client()
+    response = tester.get('/')
+    assert response.status_code == 200
+    assert b"Hello from Flask" in response.data
